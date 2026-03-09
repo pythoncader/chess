209f2d21c755347e49fe7344c817f7c799931684
pythoncader <caderigby@gmail.com>
2026-03-09 17:42:56 -0600

    Add executeUpdate method. Implement clear for users and authTokens tables

--- server/src/main/java/dataaccess/ServerUserDAO.java
+++ server/src/main/java/dataaccess/ServerUserDAO.java
@@ -5,11 +5,11 @@ import com.google.gson.Gson;
 import model.GameData;
 import model.UserData;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class ServerUserDAO implements UserDAO{
 
@@ -78,14 +78,37 @@ public class ServerUserDAO implements UserDAO{
     @Override
     public void clear() throws DataAccessException {
         var statement = "TRUNCATE chessGames";
-        try (var conn = DatabaseManager.getConnection();
-             var preparedStatement = conn.prepareStatement(statement)) {
-            preparedStatement.executeUpdate();
-        } catch (Exception ex){
-            throw new DataAccessException(ex.getMessage(), 500);
-        }
+        executeUpdate(statement);
+        statement = "TRUNCATE users";
+        executeUpdate(statement);
+        statement = "TRUNCATE authTokens";
+        executeUpdate(statement);
+
         authTokens.clear();
         users.clear();
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (Exception ex) {
+            throw new DataAccessException(ex.getMessage(), 500);
+        }
     }
 
     @Override
@@ -213,6 +236,24 @@ public class ServerUserDAO implements UserDAO{
     private final String[] createStatements = {
             """
             CREATE TABLE IF NOT EXISTS  chessGames (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS  users (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS  authTokens (
               `id` int NOT NULL AUTO_INCREMENT,
               `name` varchar(256) NOT NULL,
               `json` TEXT DEFAULT NULL,

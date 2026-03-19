2d1a0a71de7e96bba23038f24a1b8eb54ff2cf8d
pythoncader <caderigby@gmail.com>
2026-03-19 13:46:40 -0600

    Start the server with the client main class

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,21 @@
 package client;
 
 import chess.*;
+import exception.ResponseException;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String serverUrl = "http://localhost:8080";
+        if (args.length == 1) {
+            serverUrl = args[0];
+        }
+
+        try {
+            new ChessClient(serverUrl).run();
+
+        } catch (ResponseException ex) {
+            System.out.println(ex.code());
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
     }
 }

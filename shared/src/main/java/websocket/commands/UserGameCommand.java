7ee0f45da0062f72f5848bf3e9c8bd3f9e5871f8
pythoncader <caderigby@gmail.com>
2026-04-07 00:41:32 -0600

    Fixing case sensitivity for autograder

---
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -0,0 +1,67 @@
+package websocket.commands;
+
+import java.util.Objects;
+
+/**
+ * Represents a command a user can send the server over a websocket
+ * <p>
+ * Note: You can add to this class, but you should not alter the existing
+ * methods.
+ */
+public class UserGameCommand {
+
+    private final CommandType commandType;
+
+    private final String authToken;
+
+    private final Integer gameID;
+    private final String playerColor;
+
+    public UserGameCommand(CommandType commandType, String authToken, Integer gameID, String playerColor) {
+        this.commandType = commandType;
+        this.authToken = authToken;
+        this.gameID = gameID;
+        this.playerColor = playerColor;
+    }
+
+    public enum CommandType {
+        CONNECT,
+        MAKE_MOVE,
+        LEAVE,
+        RESIGN
+    }
+
+    public CommandType getCommandType() {
+        return commandType;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public Integer getGameID() {
+        return gameID;
+    }
+
+    public String getPlayerColor() {
+        return playerColor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof UserGameCommand that)) {
+            return false;
+        }
+        return getCommandType() == that.getCommandType() &&
+                Objects.equals(getAuthToken(), that.getAuthToken()) &&
+                Objects.equals(getGameID(), that.getGameID());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCommandType(), getAuthToken(), getGameID());
+    }
+}
---
+++ shared/src/main/java/websocket/messages/ServerMessage.java
@@ -0,0 +1,50 @@
+package websocket.messages;
+
+import java.util.Objects;
+
+/**
+ * Represents a Message the server can send through a WebSocket
+ * <p>
+ * Note: You can add to this class, but you should not alter the existing
+ * methods.
+ */
+public class ServerMessage {
+    ServerMessageType serverMessageType;
+
+    String message;
+
+    public enum ServerMessageType {
+        LOAD_GAME,
+        ERROR,
+        NOTIFICATION
+    }
+
+    public ServerMessage(String message, ServerMessageType type) {
+        this.message = message;
+        this.serverMessageType = type;
+    }
+
+    public ServerMessageType getServerMessageType() {
+        return this.serverMessageType;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ServerMessage that)) {
+            return false;
+        }
+        return getServerMessageType() == that.getServerMessageType();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getServerMessageType());
+    }
+}
